c1dc512ee322368d084a239dd7fa8a6cfe485a34
Brian Fischer <starfox139@yahoo.com>
2016-03-27 20:01:01 -0400

    first commit yo
    it's good

---
+++ Assets/!Scripts/GroundCheck.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundCheck : MonoBehaviour {
+
+	private PlayerControl2D player;
+
+	void Awake () {
+		player = GetComponentInParent<PlayerControl2D> ();
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		player.grounded = true;
+		player.remainingJumps = 2;
+	}
+
+	void OnTriggerStay2D(Collider2D col){
+		player.grounded = true;
+	}
+
+	void OnTriggerExit2D(Collider2D col){
+		player.grounded = false;
+	}
+}
---
+++ Assets/!Scripts/PlayerControl2D.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl2D : MonoBehaviour {
+
+	private bool jumpInput = false;
+	private float horizontalInput = 0.0f;
+
+	public float jumpStrength = 5.0f;
+	public float moveStrength = 10.0f;
+
+	private float width;
+	private float height;
+
+	public bool grounded = false;
+	public int remainingJumps = 0;
+
+
+	void Awake () {
+		width = GetComponent<BoxCollider2D> ().size.x;
+		height = GetComponent<BoxCollider2D> ().size.y;
+	}
+
+	void Update () {
+		jumpInput = (Input.GetButtonDown ("Jump") && remainingJumps >= 1 );
+		horizontalInput = Input.GetAxis ("Horizontal");
+
+		//Debug.DrawLine(bottomLeftCorner(), bottomLeftCorner() - (Vector2.up * 0.05f), Color.white, 0.0f, false);
+
+
+		print ( remainingJumps );
+
+
+	}
+
+	void FixedUpdate () {
+		Vector2 newVelocity = GetComponent<Rigidbody2D> ().velocity;
+
+		if (jumpInput) {
+			newVelocity.y = jumpStrength;
+			remainingJumps--;
+		}
+			
+		newVelocity.x = horizontalInput * moveStrength;
+
+		GetComponent<Rigidbody2D> ().velocity = newVelocity;
+	}
+
+	private Vector2 bottomLeftCorner(){
+		return new Vector2 (transform.position.x - width / 2, transform.position.y - height / 2);
+	}
+	private Vector2 bottomRightCorner(){
+		return new Vector2 (transform.position.x + width / 2, transform.position.y - height / 2);
+	}
+
+	//private bool isGrounded(){ }
+
+}
