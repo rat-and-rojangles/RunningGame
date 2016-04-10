1e986e15db25ccfdcf1eaa1adc89fff65235952d
Brian Fischer <starfox139@yahoo.com>
2016-04-10 02:06:41 -0400

    wall climb fixed, fighters imported.

--- Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
+++ Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
@@ -42,7 +42,8 @@ namespace UnityStandardAssets._2D
             Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
             for (int i = 0; i < colliders.Length; i++)
             {
-				if (colliders [i].gameObject != gameObject) {
+				//if (colliders [i].gameObject != gameObject) {
+				if (colliders [i].gameObject != gameObject && Mathf.Abs(m_Rigidbody2D.velocity.y) <= 0.1f ) {	//the thing with the velocity prevents wall climbing
 					m_Grounded = true;
 					m_RemainingJumps = k_MaxJumps;
 				}
