8d2eec796ccf11887cdc535ab88bb788cb09e34e
Brian Fischer <starfox139@yahoo.com>
2016-04-15 14:44:47 -0400

    PAUSE IS GOOD
    i lost this earlier because i forgot to back up

---
+++ Assets/!Scripts/Pause.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pause : MonoBehaviour {
+
+	//[SerializeField]
+
+	private bool paused = false;
+	private AudioLowPassFilter musicLowPass;
+
+	private const float minFQ = 450.0f;
+
+	// Use this for initialization
+	void Awake () {
+		musicLowPass = GameObject.FindGameObjectWithTag ("MusicController").GetComponent<AudioLowPassFilter> ();
+	}
+
+	void Update () {
+		if (Input.GetButtonDown ("Pause")) {
+			paused = !paused;
+
+			if (paused) {
+				Time.timeScale = 0.0f;
+				StopAllCoroutines ();
+				StartCoroutine (fadeOut ());
+			}
+			else{
+				Time.timeScale = 1.0f;
+				StopAllCoroutines ();
+				StartCoroutine (fadeIn ());
+			}
+		}
+	}
+
+	private IEnumerator fadeIn(){
+		while (musicLowPass.cutoffFrequency < 22000) {
+			print ("i");
+			musicLowPass.cutoffFrequency = Mathf.Lerp (musicLowPass.cutoffFrequency, 22000, 4 * Time.unscaledDeltaTime);
+			yield return null;
+		}
+	}
+
+	private IEnumerator fadeOut(){
+		while (musicLowPass.cutoffFrequency > minFQ) {
+			print ("o");
+			musicLowPass.cutoffFrequency = Mathf.Lerp (musicLowPass.cutoffFrequency, minFQ, 4 * Time.unscaledDeltaTime);
+			yield return null;
+		}
+	}
+}
