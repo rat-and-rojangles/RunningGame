6c614aac6ab74aeba2ea8fd4365950afcbfc804d
Brian Fischer <starfox139@yahoo.com>
2016-04-16 03:57:37 -0400

    beginning to not die for running too far forward
    too tired to work tho

--- Assets/!Scripts/Character/DieIfOffscreen.cs
+++ Assets/!Scripts/Character/DieIfOffscreen.cs
@@ -5,23 +5,27 @@ public class DieIfOffscreen : MonoBehaviour {
 
 	private Camera mainCam;
 
-	private PlatformerCharacter2D player;
+	private GameObject player;
 
 	[SerializeField] float margin = 0.15f; //remember this is viewport space, so it's this percent of a screen
 
 	void Awake () {
 		mainCam = GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<Camera> ();
 
-		player = GameObject.FindGameObjectWithTag ("Player").GetComponent<PlatformerCharacter2D> ();
+		player = GameObject.FindGameObjectWithTag ("Player");
 	}
 
 
 	void FixedUpdate () {
 		float xViewport = mainCam.WorldToViewportPoint (transform.position).x;
-		if(xViewport < -margin || xViewport > 1+margin){
+		//if(xViewport < -margin || xViewport > 1+margin){
+		if (xViewport < -margin) {
 			print ("u gone boi");
-			player.Die ();
-
+			player.GetComponent<PlatformerCharacter2D> ().Die ();
 		}
+		/*else if (xViewport > 1 + margin) {
+			Vector3 jj = new Vector3(mainCam.ViewportToWorldPoint()
+			//player.GetComponent<Rigidbody2D> ().position = mainCam.ViewportToWorldPoint()
+		}*/
 	}
 }
