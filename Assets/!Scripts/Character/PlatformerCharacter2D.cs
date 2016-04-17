b19d0b4b75a7d693e60eb5e4f396428c73bec071
Brian Fischer <starfox139@yahoo.com>
2016-04-17 19:05:36 -0400

    Cleanup

--- Assets/!Scripts/Character/PlatformerCharacter2D.cs
+++ Assets/!Scripts/Character/PlatformerCharacter2D.cs
@@ -1,11 +1,12 @@
 using System;
 using UnityEngine;
 
+using System.Collections;
+
 public class PlatformerCharacter2D : MonoBehaviour
 {
     [SerializeField] private float m_MaxSpeed = 10f;                    // The fastest the player can travel in the x axis.
     [SerializeField] private float m_JumpForce = 400f;                  // Amount of force added when the player jumps.
-    [SerializeField] private bool m_AirControl = false;                 // Whether or not a player can steer while jumping;
     [SerializeField] private LayerMask m_WhatIsGround;                  // A mask determining what is ground to the character
 
     private Transform m_GroundCheck;    // A position marking where to check if the player is grounded.
