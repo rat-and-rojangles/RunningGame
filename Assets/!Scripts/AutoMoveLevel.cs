eddd59f13d264aa0631065ff046132a4678b320e
Brian Fischer <starfox139@yahoo.com>
2016-04-17 16:58:11 -0400

    Revert "Revert "Multiple Fixes""
    This reverts commit 4469805ddff8c6b4123cf5859c307a96544e10af.

--- Assets/!Scripts/AutoMoveLevel.cs
+++ Assets/!Scripts/AutoMoveLevel.cs
@@ -1,25 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-//[ExecuteInEditMode]
 public class AutoMoveLevel : MonoBehaviour {
 
 	[SerializeField] private Vector3 direction = Vector3.right;
-	[SerializeField] private float speed = 20.0f;
+	public float speed = 20.0f;		//the character controller gets this and handles it on its own
 
-	private Rigidbody2D playerRB;
 	private GameObject mainCam;
 
 	void Awake () {
-		/*GameObject[] remaining = GameObject.FindGameObjectsWithTag("Player");
-		foreach (GameObject clone in remaining) {
-			//if(clone.name.Equals( "CharacterRobotBoy(Clone)")){
-			if(clone.name.Contains("Clone")){
-				GameObject.Destroy(clone);
-			}
-		}*/
-
-		playerRB = GameObject.FindGameObjectWithTag ("Player").GetComponent<Rigidbody2D>();
 		mainCam = GameObject.FindGameObjectWithTag ("CameraController");
 	}
 	
@@ -27,14 +16,5 @@ public class AutoMoveLevel : MonoBehaviour {
 		mainCam.transform.Translate (direction * speed * Time.deltaTime);
 	}
 
-	void FixedUpdate(){		//for player (not FixedUpdate so it's once per frame)
-		Vector3 temp = playerRB.position;
-		temp += (direction * speed * Time.fixedDeltaTime);
-		playerRB.position = temp;
-
-
-		//playerRB.MovePosition ((Vector2)playerRB.position + (Vector2)direction * speed);
-
-	}
 
 }
