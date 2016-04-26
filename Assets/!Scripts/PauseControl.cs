e12ff3ee858acb4a2b3cb364824a8d1bf07c70a4
Brian Fischer <starfox139@yahoo.com>
2016-04-25 22:10:03 -0400

    about to start slam mechanic
    Lab is the new test map

--- Assets/!Scripts/PauseControl.cs
+++ Assets/!Scripts/PauseControl.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class PauseControl : MonoBehaviour {
 
@@ -7,9 +8,14 @@ public class PauseControl : MonoBehaviour {
 	private AudioLowPassFilter musicLowPass;
 	[SerializeField] private float musicMinFQ = 450.0f;
 
+	private Vector3 camLastEuler;
+
+	private Transform camTrans;
+
 	// Use this for initialization
 	void Awake () {
 		musicLowPass = GameObject.FindGameObjectWithTag ("MusicController").GetComponent<AudioLowPassFilter> ();
+		camTrans = GameObject.FindGameObjectWithTag ("CameraController").transform.Find ("CamOffset").transform;
 	}
 
 	public bool IsPaused(){
@@ -28,6 +34,9 @@ public class PauseControl : MonoBehaviour {
 	public void Pause(){
 		paused = true;
 		Time.timeScale = 0.0f;
+
+		camLastEuler = camTrans.rotation.eulerAngles;
+
 		StopAllCoroutines ();
 		StartCoroutine (fadeOut ());
 	}
@@ -52,5 +61,19 @@ public class PauseControl : MonoBehaviour {
 			yield return null;
 		}
 		musicLowPass.cutoffFrequency = musicMinFQ;
+	}
+
+	void Update(){
+		/*if (paused) {	//krazykam dummied out
+			Quaternion tempQ = camTrans.rotation;
+
+			Vector3 tempEuler = tempQ.eulerAngles;
+			tempEuler += Vector3.up * CrossPlatformInputManager.GetAxisRaw("Horizontal") * Time.unscaledDeltaTime * 100;
+			tempEuler += Vector3.right * CrossPlatformInputManager.GetAxisRaw("Vertical") * Time.unscaledDeltaTime * 100;
+			tempQ.eulerAngles = tempEuler;
+
+			tempQ.eulerAngles = tempEuler;
+			camTrans.rotation = tempQ;
+		}*/
 	}
 }
